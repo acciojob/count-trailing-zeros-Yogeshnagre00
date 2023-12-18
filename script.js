38b30e23eded4f8910c6b0426fb71cd3468837da
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-12-18 15:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let factorial = 1;
+	for (let i = 1; i <= n; i++) {
+		factorial*=i;
+	}
+	let count = 0;
+	while (factorial!==0) {
+		let rem = factorial%10;
+		if (rem == 0) {
+			count++;
+		}else{
+			return count
+		}
+		factorial= Math.floor(factorial/10);
+	}
+	return count
 }
 
 const input = prompt("Enter a number");
+
+
 alert(trailingZeros(input));
